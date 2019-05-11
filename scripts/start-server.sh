3fd450ce4cc80b38d072270f17b5618127f0e756
kirito92 <kirito1992@users.noreply.github.com>
2019-05-10 21:57:16 -0400

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -56,7 +56,23 @@ sleep infinity
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-./server ${GAME_PARAMS} +port ${GAME_PORT}
+#!/bin/bash
+cd /home/steam/wu
+
+if [ -f ./server.cfg ]; then  
+  source ./server.cfg
+else  
+  echo "Unable to find server.cfg file. Quitting like a coward."
+  exit 1
+fi
+
+if [ -f WurmServerLauncher-patched ]; then  
+  service_file='./WurmServerLauncher-patched'
+else  
+  service_file='./WurmServerLauncher'
+fi
+
+$service_file ADMINPWD="$ADMINPWD" EPICSETTINGS="$EPICSETTINGS" EXTERNALPORT="$EXTERNALPORT" HOMESERVER="$HOMESERVER" HOMEKINGDOM="$HOMEKINGDOM" LOGINSERVER="$LOGINSERVER" MAXPLAYERS="$MAXPLAYERS" QUERYPORT="$QUERYPORT" SERVERNAME="$SERVERNAME" SERVERPASSWORD="$SERVERPASSWORD" START="$START" IP="$IP"
 
 
 
